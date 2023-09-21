7addddcad9190cc4b7df8a0f09b691d6620ff474
Andreas Marcussen Damm <andr374n@mail.hansenberg.dk>
2023-09-21 11:24:35 +0200

    HitBox Alien Man
    hitbox alienman

---
+++ Assets/AlienMan.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienMan : Player
+{
+    public Hand LeftHand;
+
+    public Hand RightHand;
+
+    public Foot LeftFoot;
+
+    public Foot RightFoot;
+
+    public Body MainBody;
+
+    public Head MainHead;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
